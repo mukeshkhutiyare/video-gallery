d2d319ab81737af0d5f0819b130dbc50fc66cf44
mukesh khutiyare <mukeshdb.air@gmail.com>
2024-05-25 21:18:32 +0530

    add

---
+++ socket.server.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const express = require('express');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('offer', (data) => {
+    socket.broadcast.emit('offer', data);
+  });
+
+  socket.on('answer', (data) => {
+    socket.broadcast.emit('answer', data);
+  });
+
+  socket.on('candidate', (data) => {
+    socket.broadcast.emit('candidate', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
